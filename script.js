f90969710e43d1590ed1777d272e62e21a5ff2d1
Jack Naylor <jacknaylor999@hotmail.com>
2026-02-05 10:25:18 +0700

    win conditions and reset

--- script.js
+++ script.js
@@ -3,24 +3,28 @@ const playerOneCurrentScore = document.getElementById('current--0');
 const playerTwoCurrentScore = document.getElementById('current--1');
 const roll = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
+const newGame = document.querySelector('.btn--new');
 const playerOneScore = document.getElementById('score--0');
 const playerTwoScore = document.getElementById('score--1');
 const playerOneActive = document.querySelector('.player--0');
 const playerTwoActive = document.querySelector('.player--1');
 const diceImg = document.querySelector('.dice');
+
 let tempScore = 0;
 let playerOneTotalScore = 0;
 let playerTwoTotalScore = 0;
-
+let gameOver = false;
 let diceNumber = 0;
 
 roll.addEventListener('click', () => {
+  if (gameOver) return;
   rollDice();
   checkForOne(diceNumber);
   displayDiceRoll(diceNumber, tempScore);
 });
 
 hold.addEventListener('click', () => {
+  if (gameOver) return;
   if (playerOneActive.classList.contains('player--active')) {
     playerOneTotalScore = playerOneTotalScore + tempScore;
     playerOneScore.textContent = playerOneTotalScore;
@@ -34,14 +38,35 @@ hold.addEventListener('click', () => {
     playerTwoActive.classList.remove('player--active');
     tempScore = 0;
   }
-
-  if (playerOneTotalScore > 100) {
-    console.log('PLayer one wines');
-  } else if (playerTwoTotalScore > 100) {
-    console.log('player two wins');
-  }
+  checkForWinner();
 });
 
+newGame.addEventListener('click', () => {
+  gameOver = false;
+  tempScore = 0;
+  playerOneTotalScore = 0;
+  playerTwoTotalScore = 0;
+  playerOneActive.classList.remove('player--winner');
+  playerOneCurrentScore.textContent = 0;
+  playerOneScore.textContent = 0;
+  playerTwoScore.textContent = 0;
+  playerTwoCurrentScore.textContent = 0;
+  playerTwoActive.classList.remove('player--winner');
+  console.log('NEW GAME');
+});
+
+function checkForWinner() {
+  if (playerOneTotalScore >= 10) {
+    console.log('PLayer one wins');
+    playerOneActive.classList.add('player--winner');
+    tempScore = 0;
+  } else if (playerTwoTotalScore >= 10) {
+    console.log('player two wins');
+    playerTwoActive.classList.add('player--winner');
+    tempScore = 0;
+  }
+  gameOver = true;
+}
 function rollDice() {
   diceNumber = Math.floor(Math.random() * 6) + 1;
   console.log('Dice roll is ' + diceNumber);
